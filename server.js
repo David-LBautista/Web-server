0b88762e004dc2ceac63a0bb5f9404510f098b20
David-LBautista <196w0007@itszongolica.edu.mx>
2020-02-15 23:05:20 -0600

    heroku

--- server.js
+++ server.js
@@ -1,21 +1,37 @@
 const express = require('express');
 const app = express();
- 
-app.get('/', (req, res) => {
-  //res.send('Hello World');
-  let salida = {
-    nombre: 'David',
-    edad: 30,
-    url: req.url
-    };
 
-    res.send(salida);
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+//obtener puerto de heroku
+const port = process.env.PORT || 3000;
+
+
+
+app.use( express.static(__dirname + '/public') );
+
+//Express HBS engine
+hbs.registerPartials( __dirname + '/views/partials' );
+app.set('view engine', 'hbs');
+
+ app.get('/', (req, res) => {
+
+     res.render('home', {
+         nombre: 'david luCAs bauTISTA'
+         
+     });
+ });
+
+ app.get('/about', (req, res) => {
+
+    res.render('about');
 });
 
 app.get('/data', (req, res) => {
     res.send('Hello World');
   });
- 
-app.listen(3000, () => {
-    console.log('Escuchando en el puerto 3000');
+
+app.listen(port, () => {
+    console.log(`Escuchando en el puerto ${ port }`);
 });
